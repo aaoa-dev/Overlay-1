286fe4813bc9edf5b359c04deaa1870e84469d97
Snorl4xX7 <anatoleayadi@gmail.com>
2025-03-23 11:35:41 +0100

    Example

--- config.example.js
+++ config.example.js
@@ -3,13 +3,10 @@ export const config = {
         TWITCH: {
             USERNAME: '',
             OAUTH_TOKEN: '',
-            CHANNEL_NAME: ''
+            CHANNEL_NAME: '',
+            CHANNEL_ID: ''
         },
         YOUTUBE: {
-            // Your YouTube channel name 
-            // This is the name that appears in your YouTube URL
-            // Example: For youtube.com/c/YourChannel, enter "YourChannel"
-            // Example: For youtube.com/@yourhandle, enter "yourhandle"
             CHANNEL_NAME: ''
         }
     },
